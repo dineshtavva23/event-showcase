dc1487ed002e7b2c2b4f1b487442a4aec653d2e2
dineshtavva23 <dineshtavva20@gmail.com>
2025-06-10 08:40:56 +0000

    added css for events page and pages are completed

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ const themeToggle = document.getElementById('toggle-btn');
 const body = document.body;
 
 themeToggle.addEventListener('click', () => {
-  // Toggle class on body
   if (body.classList.contains('light-theme')) {
     body.classList.remove('light-theme');
-    // Optional: Change icon to moon
     themeToggle.innerHTML = '<i class="fas fa-moon"></i>';
   } else {
     body.classList.add('light-theme');
-    // Optional: Change icon to sun
     themeToggle.innerHTML = '<i class="fas fa-sun"></i>';
   }
 });
